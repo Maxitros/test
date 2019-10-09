ce054ca5fc86b6aec07830140485f3be76139fea
Maxitros <47903625+Maxitros@users.noreply.github.com>
2019-10-09 10:12:50 +0100

    Create ces.py

---
+++ ces.py
@@ -0,0 +1,46 @@
+a1=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',\
+'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+
+a2=[]
+
+key = int(input('Key>> '))
+for i in range(len(a1)) :
+	a2.append(a1[i])
+
+for i in range(key) :
+	a2.append(a2[0])
+	a2.remove(a2[0])
+
+inp = input("Write the text>> ")
+
+out = '' 
+
+for n in inp :
+	for i in range(len(a1)) :
+		if n == a1[i] :
+			out += a2[i]
+			break
+	else : 
+		out += n
+
+out_sp = out.split(' ')
+out_sp1 = []
+out_sp2 = []
+for integer in out_sp : 
+	try :
+		integer = int(integer)
+		integer += key
+		out_sp1.append(integer)
+	except :
+		out_sp1.append(integer)
+
+
+for n in out_sp1 : 
+	l = str(n)
+	out_sp2.append(l)
+
+out_final = ' '.join(out_sp2)
+ 
+
+print(out_final)
